fc4415a310b20cae3e9423cd1c1ed394a6fac453
Gabriel Mendes <gabriel.mendes17@outlook.com>
2024-07-28 19:18:10 -0300

    feat: create PedidoContext for global management state of api data

---
+++ src/app/context/PedidoContext.tsx
@@ -0,0 +1,55 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode
+} from 'react'
+import axios from 'axios'
+import { Pedido } from '../models/pedido'
+
+interface PedidoContextType {
+  pedidos: Pedido[]
+  loading: boolean
+}
+
+const PedidoContext = createContext<PedidoContextType | undefined>(undefined)
+
+export const usePedido = (): PedidoContextType => {
+  const context = useContext(PedidoContext)
+  if (!context) {
+    throw new Error('usePedido must be used within a PedidoProvider')
+  }
+  return context
+}
+
+interface PedidoProviderProps {
+  children: ReactNode
+}
+
+export const PedidoProvider: React.FC<PedidoProviderProps> = ({ children }) => {
+  const [pedidos, setPedidos] = useState<Pedido[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPedidos = async () => {
+      try {
+        const response = await axios.get('http://localhost:5245/api/pedido')
+        console.log(response.data.dados)
+        setPedidos(response.data.dados)
+      } catch (error) {
+        console.error('Erro ao buscar pedidos:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPedidos()
+  }, [])
+
+  return (
+    <PedidoContext.Provider value={{ pedidos, loading }}>
+      {children}
+    </PedidoContext.Provider>
+  )
+}
---
+++ src/app/models/pedido.ts
@@ -0,0 +1,7 @@
+export interface Pedido {
+  id: number
+  value: number
+  date: number
+  formOfPayment: string
+  status: string
+}
